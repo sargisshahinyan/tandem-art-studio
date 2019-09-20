bea421fc6de790e182772ecd1c546f7a3867c667
Sargis <shahinyan.sargis@gmail.com>
2019-09-20 20:35:19 +0400

    Integrated contacts page

--- front-end/src/components/Contacts/index.js
+++ front-end/src/components/Contacts/index.js
@@ -1,9 +1,53 @@
 import React from 'react';
 
+import Header from '../Header';
+import BasicFooter from '../BasicFooter';
+
 import './styles.scss';
 
 export function Contacts() {
-	return <h1>Contacts</h1>;
+  return (
+    <article>
+      <div className="bg_sim_styles bg_contact_us" />
+      <Header />
+      <main className="contact_us">
+        <div className="title">
+          <div className="wrapper">
+            <h1>Contact with us</h1>
+          </div>
+        </div>
+        <div className="form_content">
+          <div className="wrapper">
+            <div className="content">
+              <div className="inputs">
+                <div>
+                  <input placeholder="Name:" type="text" />
+                </div>
+                <div>
+                  <input placeholder="Surname:" type="text" />
+                </div>
+                <div>
+                  <input placeholder="Email:" type="text" />
+                </div>
+                <div>
+                  <input placeholder="Phone:" type="text" />
+                </div>
+              </div>
+              <div className="message">
+                <div className="message_content">
+                  <textarea placeholder="Message:" />
+                </div>
+                <div className="send_button_position">
+                  <button type="submit">Send</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <BasicFooter/>
+    </article>
+  );
 }
 
 export default Contacts;
